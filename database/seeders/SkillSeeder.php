3acd2793eba683706bf619894199368712709dee
Sozi Fatima <B00158031@mytudublin.ie>
2025-04-16 19:01:29 +0100

    Display seeded skills on /skills page with controller and Blade view

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -3,19 +3,17 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Skill;
 
-class DatabaseSeeder extends Seeder
+class SkillSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            SkillSeeder::class,
-        ]);
+        Skill::factory()->count(10)->create();
     }
 }
-
